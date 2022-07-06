4d5ec14631900815feee125ffa29fd75cf773d73
scharlez <stevencharlez@gmail.com>
2022-07-06 15:00:06 -0700

    Tic-Tac Throwback

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 
 class Square extends Component {
+
+handleClick = ()=> {
+  alert(this.props.index)
+}
+
+
   render() {
     return(
       <>
-        <div className="square"></div>
+        <div className='square' onClick={this.handleClick}></div>
       </>
     )
   }
