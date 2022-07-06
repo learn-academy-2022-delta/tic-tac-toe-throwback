88b0e5546067483f55344b1dc7fe2b894757ce26
LeoContreras93 <Contreras.leo2211@gmail.com>
2022-07-06 16:55:39 -0700

    styling-changes

--- src/components/Square.js
+++ src/components/Square.js
@@ -3,14 +3,15 @@ import React, { Component } from 'react'
 class Square extends Component {
 
 handleClick = ()=> {
-  alert(this.props.index)
+  // alert(this.props.index)
+  this.props.handleGamePlay(this.props.index)
 }
 
 
   render() {
     return(
       <>
-        <div className='square' onClick={this.handleClick}></div>
+        <div className='square' onClick={this.handleClick}>{this.props.value}</div>
       </>
     )
   }
